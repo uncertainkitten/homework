d37673f7548f2ec6719b75a4f5a471a057be0af8
Cassandra <uncertainkitten@gmail.com>
2018-08-24 02:34:00 -0400

    Add Stack#push method

---
+++ W1D5/stack.rb
@@ -0,0 +1,17 @@
+class Stack
+  def initialize(stack)
+    @stack = stack
+  end
+
+  def push(el)
+    @stack.push(el)
+  end
+
+  def pop
+    # removes one element from the stack
+  end
+
+  def peek
+    # returns, but doesn't remove, the top element in the stack
+  end
+end
