a3562568e00ff9854c8b74fb69bad375e3d3a7f4
Cassandra <uncertainkitten@gmail.com>
2018-08-24 03:18:42 -0400

    Create and debug Map#delete - deletes items and repairs map if empty

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -8,12 +8,11 @@ class Map
   end
 
   def set(key, value)
-    found = false
-
-    if @map[0].empty? && @map.length <= 1
-      @map[0] = [key, value]
+    if if_empty?
+      return @map[0] = [key,value]
     end
 
+    found = false
     @map.each do |el|
       if el[0] == key
         el[1] = value
@@ -34,7 +33,30 @@ class Map
     return nil
   end
 
+  def if_empty?
+    if @map.empty? && @map.length <= 1
+      @map[0] = []
+      return true
+    elsif @map[0].empty? && @map.length <= 1
+      return true
+    else
+      return false
+    end
+  end
+
   def delete(key)
+    return_val = nil
+    @map.each_with_index do |el, idx|
+      if el[0] == key
+        return_val = @map.delete(@map[idx])
+      end
+    end
+
+    if if_empty?
+      return @map
+    else
+      return return_val
+    end
   end
 
   def show
