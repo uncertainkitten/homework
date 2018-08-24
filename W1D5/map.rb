3662f1fb38f13d6c654f6b3a133278d0b84b765b
Cassandra <uncertainkitten@gmail.com>
2018-08-24 02:52:14 -0400

    Fix Map#initialize conditional; Create Map#set to respect uniqueness of keys

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,16 +1,26 @@
 class Map
   def initialize(map = nil)
-    if map = nil
+    if map == nil
       @map = [[]]
     else
-      @map == map
+      @map = map
     end
   end
 
   def set(key, value)
+    found = false
+    @map.each do |el|
+      if el[0] == key
+        el[1] = value
+        found = true
+        return value
+      end
+    end
+    @map.push([key, value])
   end
 
   def get(key)
+
   end
 
   def delete(key)
