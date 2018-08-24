6fe5a4e596ea08df64d45362d0b15db799d7f1d5
Cassandra <uncertainkitten@gmail.com>
2018-08-24 02:41:02 -0400

    Define Queue#initialize with default value

---
+++ W1D5/queue.rb
@@ -0,0 +1,18 @@
+class Queue
+  def initialize(queue= nil)
+    if queue == nil
+      @queue = []
+    else
+      @queue = queue
+    end
+  end
+
+  def enqueue(el)
+  end
+
+  def dequeue
+  end
+
+  def peek
+  end
+end
