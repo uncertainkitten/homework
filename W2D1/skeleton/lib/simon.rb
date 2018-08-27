e5abbb4a933fb8a44fc28292404bfdaf4d8e961a
Cassandra <uncertainkitten@gmail.com>
2018-08-26 20:54:34 -0400

    Pass initialize specs; pass add_random_color specs; pass take_turn specs; pass play specs

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -4,15 +4,27 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
+    take_turn while game_over == false
 
+    if game_over == true
+      game_over_message
+      reset_game
+    end
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    if game_over == false
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
@@ -24,7 +36,7 @@ class Simon
   end
 
   def add_random_color
-
+    seq.push(COLORS.sample)
   end
 
   def round_success_message
