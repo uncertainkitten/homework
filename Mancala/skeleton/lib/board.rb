642c1827ccc4afc35a4b62571d60cb13be4efb3d
Cassandra <uncertainkitten@gmail.com>
2018-08-28 02:14:35 -0400

    Pass most specs on Mandala project - Board#next_turn need bugfix

--- Mancala/skeleton/lib/board.rb
+++ Mancala/skeleton/lib/board.rb
@@ -28,30 +28,48 @@ class Board
 
   def make_move(start_pos, current_player_name)
     handful = []
-    empty_cup = @cups[start_pos]
+    if start_pos < 6 && start_pos > 0
+      act_pos = start_pos - 1
+    else
+      act_pos = start_pos
+    end
+
+
+    empty_cup = @cups[act_pos]
     empty_cup.length.times do |i|
       handful << empty_cup.pop
     end
 
-    dist = (start_pos + 1) % 13
+    dist = (act_pos + 1)
     until handful.empty?
       if current_player_name == @name1 && dist == 13
-        dist = (dist + 1) % 13
+        dist = (dist + 1) % 13 unless handful.empty?
       elsif current_player_name == @name2 && dist == 6
-        dist = (dist + 1) % 13
+        dist = (dist + 1) unless handful.empty?
+      elsif dist == 13
+        @cups[dist] << handful.pop
+        dist = (dist + 1) % 13 unless handful.empty?
       else
         @cups[dist] << handful.pop
-        dist = (dist + 1) % 13
+        dist = (dist + 1) unless handful.empty?
       end
     end
 
     render
+    next_turn(dist)
 
 
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].length < 2
+      :switch
+    else
+      ending_cup_idx
+    end
   end
 
   def render
@@ -63,9 +81,25 @@ class Board
   end
 
   def one_side_empty?
+    side1 = @cups[0..5]
+    side2 = @cups[7..12]
+
+    if side1.all? {|el| el.empty?} || side2.all? {|el| el.empty?}
+      return true
+    else
+      return false
+    end
+
   end
 
   def winner
+    if @cups[6].length == @cups[13].length
+      :draw
+    elsif @cups[6].length > @cups[13].length
+      @name1
+    else
+      @name2
+    end
   end
 end
 
