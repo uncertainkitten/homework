e53b7fe42de720edc42569aaa59c87c037b61011
Cassandra <uncertainkitten@gmail.com>
2018-09-05 21:00:46 -0700

    Finish W3D4 homework

--- active_record_warmup/movie_novice/queries.rb
+++ active_record_warmup/movie_novice/queries.rb
@@ -22,24 +22,20 @@
 
 
 def find_angelina
-  #find Angelina Jolie by name in the actors table
-
+  Actor.find_by(name: "Angelina Jolie")
 end
 
 def top_titles
-  # get movie titles from movies with scores greater than or equal to 9
-  # hint: use 'select' and 'where'
-
+  Movie.select("id, title").where("score >= 9")
 end
 
 def star_wars
-  #display the id, title and year of each Star Wars movie in movies.
-  # hint: use 'select' and 'where'
-
+  Movie.select("id, title, yr").where("title like ?", "#{"Star Wars"}%")
 end
 
 
 def below_average_years
+  Movie.select("yr, COUNT(*) AS bad_movies").where("score < 5").group("yr").order("bad_movies DESC")
   #display each year with movies scoring under 5,
   #with the count of movies scoring under 5 aliased as bad_movies,
   #in descending order
@@ -48,15 +44,11 @@ def below_average_years
 end
 
 def alphabetized_actors
-  # display the first 10 actor names ordered from A-Z
-  # hint: use 'order' and 'limit'
-  # Note: Ubuntu users may find that special characters
-  # are alphabetized differently than the specs.
-  # This spec might fail for Ubuntu users. It's ok!
-
+  Actor.order(:name).limit(10)
 end
 
 def pulp_fiction_actors
+  Actor.select("actors.id, actors.name").joins(:movies).where("movies.title = ?", "Pulp Fiction")
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
@@ -64,9 +56,5 @@ def pulp_fiction_actors
 end
 
 def uma_movies
-  #practice using joins
-  # display the id, title, and year of movies Uma Thurman has acted in
-  # order them by ascending year
-  # hint: use 'select', 'joins', 'where', and 'order'
-
+  Movie.select("movies.id, movies.title, movies.yr").joins(:actors).where("actors.name = ?", "Uma Thurman").order("movies.yr ASC")
 end
